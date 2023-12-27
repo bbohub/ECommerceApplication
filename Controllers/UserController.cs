42a86d55cc59875259c5ca4501d9810c79dc0d35
bilahl <bilahl.bo@gmail.com>
2023-12-27 21:02:13 +0100

    Ajoutez des fichiers projet.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceApplication.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: UserController
+        public String Index()
+        {
+            return "This the first user";
+        }
+        
+        public String TestPathUser()
+        {
+            return "this is the test of the path of an user";
+        }
+
+    }
+}
